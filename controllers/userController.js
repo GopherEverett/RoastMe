205f61be33ad51da0632d8f91e197a6f81121466
John Jacobs <john.a.jacobs1973@gmail.com>
2019-03-04 11:36:38 -0500

    all user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,24 @@
 const userController = {
     index: (req,res) => {
         res.send('Index Page')
+    },
+    new: (req,res) => {
+        res.send('New Form')
+    },
+    create: (req,res) => {
+        res.send('New User')
+    },
+    show: (req,res) => {
+        res.send('User #1')
+    },
+    edit: (req,res) => {
+        res.send('form to edit user')
+    },
+    update: (req,res) => {
+        res.send('updated user')
+    },
+    delete: (req,res) => {
+        res.send('deleted user')
     }
 }
 module.exports = userController 
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,12 @@ const router = express.Router()
 const userController = require('../controllers/userController.js')
 
 router.get('/', userController.index)
+router.get('/new', userController.new)
+router.post('/', userController.create)
+router.get('/:userId', userController.show)
+router.get('/:userId/edit', userController.edit)
+router.put('/:userId', userController.update)
+router.delete('/:userId', userController.delete)
+
 
 module.exports = router
