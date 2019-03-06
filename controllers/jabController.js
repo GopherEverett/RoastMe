f9c6adf55931b766cc3809f6426afe755efa9272
John Jacobs <john.a.jacobs1973@gmail.com>
2019-03-06 16:53:17 -0500

    form styling

--- controllers/jabController.js
+++ controllers/jabController.js
@@ -3,48 +3,52 @@ const Roast = require('../models/Roast')
 const Jabs = require('../models/Jab')
 
 const jabController = {
-    index: (req,res) => {
+    index: (req, res) => {
         res.send('Jabs')
     },
-    new: (req,res) => {
+    new: (req, res) => {
         Roast.findById(req.params.roastId)
-        .then((roast) =>{
-            res.render('jabs/new', {roast})
+            .then((roast) => {
+                res.render('jabs/new', { roast })
+            })
+    },
+    create: (req, res) => {
+        Roast.findById(req.params.roastId)
+            .then((roast) => {
+                Jabs.create(req.body).then(jab => {
+                    roast.jabs.push(jab)
+                    roast.save()
+                    res.redirect(`/${req.params.roastId}/jabs/${jab._id}`)
+                })
+            })
+    },
+    show: (req, res) => {
+        Jabs.findById(req.params.jabId)
+            .then((jab) => {
+                res.render('jabs/show', { jab })
+            })
+    },
+    edit: (req, res) => {
+        Jabs.findById(req.params.jabId).then((jab) => {
+            res.render('jabs/edit', { jab })
         })
     },
-    create: (req,res) => {
+    update: (req, res) => {
         Roast.findById(req.params.roastId)
-        .then((roast) => {
-            Jabs.create(req.body).then(jab => {
+        then((roast) => {
+            Jabs.findByIdAndUpdate(req.params.jabId, req.body, { new: true }).then((jab) => {
                 roast.jabs.push(jab)
                 roast.save()
-                res.redirect(`/${req.params.roastId}/jabs/${jab._id}`)
-            })  
-        })
-    },
-    show: (req,res) => {
-        Jabs.findById(req.params.jabId)
-        .then((jab) => {
-            res.render('jabs/show', {jab})
-        })
-    },
-    edit: (req,res) => {
-        Jabs.findById(req.params.jabId).then(jab => {
-        res.render('jabs/edit', {jab})
-        })   
-    },
-    update: (req,res) => {
-        Jabs.findByIdAndUpdate(req.params.jabId, req.body, {new: true}).then((update) =>{
-            console.log(update)
-            // res.redirect(`/${req.params.jabId}`)
-        })
-    },
-    delete: (req,res) => {
-        Jabs.findByIdAndDelete(req.params.jabId).then(() => {
-            console.log(`deleted ${req.params.jabId}`)
-            res.redirect('/users')
-        })
-    }
+                res.redirect(`jabs/${req.params.jabId}`)
+            })
+         })
+        },
+            delete: (req, res) => {
+                Jabs.findByIdAndDelete(req.params.jabId).then(() => {
+                    console.log(`deleted ${req.params.jabId}`)
+                    res.redirect('/user')
+                })
+            }
 }
 
 module.exports = jabController
